e2bba14187f45608259023ca9cd6ab712a63d46f
Aman Jain <aman.jain2021@vitbhopal.ac.in>
2022-04-17 05:36:31 +0530

    Added base result xml file, and fixed bugs

--- app/src/main/java/com/broood/quiz/Result.kt
+++ app/src/main/java/com/broood/quiz/Result.kt
@@ -1,7 +1,11 @@
 package com.broood.quiz
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class Result : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.result)
+    }
 }
